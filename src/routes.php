bbda97f312cf6fd69b0ce5ace132ca45c862f2cd
Mohammad <mohammad@lync.me>
2022-02-23 20:35:51 +0100

    Initial routes & Added gitignore

--- src/routes.php
+++ src/routes.php
@@ -1,1 +1,12 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use Moh6mmad\LaravelSettings\Controllers\LaravelSettingsController;
+
+Route::any('/settings/delete/{settingId}', [LaravelSettingsController::class, 'delete'])->name('settings.delete');
+Route::resource('settings', LaravelSettingsController::class, [
+    'names' => [
+        'index' => 'settings',
+        'store' => 'settings.new',
+    ]
+]);
