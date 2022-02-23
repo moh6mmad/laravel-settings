ae91a75fb60cb9256948dd72a47e07384055c1d4
Mohammad <mohammad@lync.me>
2022-02-23 19:57:27 +0100

    Updated router, deleted unnecessary routes

--- src/routes.php
+++ src/routes.php
@@ -1,4 +1,1 @@
 <?php
-
-Route::get('add/{a}/{b}', 'Moh6mmad\LaravelSettings\Controllers\LaravelSettingsController@get');
-Route::get('subtract/{a}/{b}', 'Moh6mmad\LaravelSettings\Controllers\LaravelSettingsController@subtract');
