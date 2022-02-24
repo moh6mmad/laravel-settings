0c9d52691e819f2f1cb6b1e6e7f97410d939b3a9
Mohammad <mohammad@lync.me>
2022-02-24 18:21:39 +0100

    Fixed helper location, Updated composer json

--- src/Helpers/Settings.php
+++ src/Helpers/Settings.php
@@ -1,17 +1,18 @@
 <?php
 
 use Moh6mmad\LaravelSettings\Models\LaravelSettings;
-
+if (!function_exists('setting')) {
 /**
  * This function allows app to get directly settings from database
  * settings should be called like setting('setting_group.setting_key');
  *
  * @param string $key
  * @param mixed $value
- * 
+ *
  * @return array|string|void
  */
-function setting(string $key = '', $value = '')
-{
-    LaravelSettings::setting($key, $value);
+    function setting(string $key = '', $value = '')
+    {
+        LaravelSettings::setting($key, $value);
+    }
 }
