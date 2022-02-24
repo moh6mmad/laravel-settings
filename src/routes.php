7d634ff6c34a9709b452b9033df54400e5049de8
Mohammad <mohammad@lync.me>
2022-02-24 18:44:13 +0100

    Temporary delete route & views, Todo make them publishable

--- src/routes.php
+++ src/routes.php
@@ -1,12 +1,1 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-use Moh6mmad\LaravelSettings\Controllers\LaravelSettingsController;
-
-Route::any('/settings/delete/{settingId}', [LaravelSettingsController::class, 'delete'])->name('settings.delete');
-Route::resource('settings', LaravelSettingsController::class, [
-    'names' => [
-        'index' => 'settings',
-        'store' => 'settings.new',
-    ]
-]);
